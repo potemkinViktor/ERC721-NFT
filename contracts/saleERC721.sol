ef7d27c470beabc1cc7a03d5faf08b89e3074bc2
potemkinViktor <viktorapotemkin@gmail.com>
2022-03-29 16:21:18 +0300

    new files

--- contracts/saleERC721.sol
+++ contracts/saleERC721.sol
@@ -10,7 +10,7 @@ contract saleERC721 is Ownable, ERC721Enumerable {
     uint256 public costPublic = 0.07 ether;
     uint256 public maxSupplyPresale = 1000;
     uint256 public maxSupply = 5000;
-    uint256 public maxMintAmountPresale = 3; 
+    uint256 public maxMintAmountPresale = 3; //не выгоднее ли по газу просто использовать число в require
     uint256 public maxMintAmountPublic = 10; 
     uint256 public deployTime;
     uint256 public presaleAddresses = 0;
@@ -25,12 +25,59 @@ contract saleERC721 is Ownable, ERC721Enumerable {
     }
 
     function mint(uint256 _mintAmount) public payable {
-        require(msg.sender != owner, "Owner couldn't mint!");
-        if(block.timestamp < 2 days + deployTime, "Pre sale is over");
-        uint256 supply = totalSupply();
-        require(_mintAmount > 0, "You should mint more than 0 NFT");
-        require(addressMintedBalance + _mintAmount < maxMintAmountPresale, "")
-        require();
-        require(!alreadyMintedPresale,"");
+        require(msg.sender != owner(), "Owner couldn't mint!");
+        if(block.timestamp < 2 days + deployTime && maxSupplyPresale > 0 ) {
+            uint256 supply = totalSupply();
+            require(_mintAmount > 0, "You should mint more than 0 NFT");
+            require(addressMintedBalance[msg.sender] + _mintAmount < maxMintAmountPresale, "Limit of presale NFT for one address is over");
+            require(supply + _mintAmount < maxSupplyPresale, "Limit of presale NFT is over or try to mint less");
+            require(msg.value >= costPresale*_mintAmount, "Sorry you don't have enough money");
+
+            for(uint256 i=1; i<= _mintAmount; i++) {
+                addressMintedBalance[msg.sender]++;
+                _safeMint(msg.sender, supply+i);
+            }
+
+            if(msg.value > costPresale*_mintAmount) {
+                payable(msg.sender).transfer(msg.value-(costPresale*_mintAmount));
+            }
+
+            if(!alreadyMintedPresale[msg.sender] == true) {
+                presaleAddresses += 1;
+                alreadyMintedPresale[msg.sender] = true;
+            }
+        } else {
+            require(maxSupply > 0, "Public sale is over");
+            uint256 supply = totalSupply();
+            require(_mintAmount > 0, "You should mint more than 0 NFT");
+            require(addressMintedBalance[msg.sender] + _mintAmount < maxMintAmountPublic, "Limit of public sale NFT for one transaction");
+            require(supply + _mintAmount < maxSupply, "Limit of public sale NFT is over or try to mint less");
+            require(msg.value >= costPublic*_mintAmount, "Sorry you don't have enough money");
+
+            for(uint256 i=1; i<= _mintAmount; i++) {
+                addressMintedBalance[msg.sender]++;
+                _safeMint(msg.sender, supply+i);
+            }
+
+            if(msg.value > costPresale*_mintAmount) {
+                payable(msg.sender).transfer(msg.value-(costPresale*_mintAmount));
+            }
+        } 
+    }
+
+    function airdrop(address[] memory _users) public onlyOwner {
+    require(_users.length == 100, "Only 100 users");
+    uint256 supply = totalSupply();
+      for (uint256 i = 0; i < _users.length; i++) {
+        addressMintedBalance[_users[i]]++;
+        _safeMint(_users[i], supply + i);
+      }
+  }
+ 
+  function withdraw(address to, uint256 amount) public onlyOwner {
+        require(to != address(0), "Wrong address");
+        payable(to).transfer(amount);
+
+        emit Withdraw(to, amount);
     }
 }
